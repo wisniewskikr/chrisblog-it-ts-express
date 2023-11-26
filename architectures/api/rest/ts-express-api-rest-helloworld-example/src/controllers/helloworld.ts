55580478de2610a0b97694d1c9353846e473c6d4
wisniewskikr <wisniewskikr@gmail.com>
2023-11-26 17:17:54 +0100

    Add init message

--- architectures/api/rest/ts-express-api-rest-helloworld-example/src/controllers/helloworld.ts
+++ architectures/api/rest/ts-express-api-rest-helloworld-example/src/controllers/helloworld.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 
-export const getMessageIndex = (req: Request, res: Response): void => {
-    res.send("Hello World Index");
+export const handleIndex = (req: Request, res: Response): void => {
+    res.send("Please use following URL structure: http://localhost:3000/name/{name} or http://localhost:3000/name/{name}?contentType=json");
 };
 
-export const getMessageHelloWorld = (req: Request, res: Response): void => {
+export const handleName = (req: Request, res: Response): void => {
     res.send("Hello World Message");
 };
--- architectures/api/rest/ts-express-api-rest-helloworld-example/src/router/index.ts
+++ architectures/api/rest/ts-express-api-rest-helloworld-example/src/router/index.ts
@@ -1,12 +1,12 @@
-import { getMessageIndex, getMessageHelloWorld } from "../controllers/helloworld";
+import { handleIndex, handleName } from "../controllers/helloworld";
 import { getResourceNotFound } from "../controllers/error";
 import express, { Router } from "express";
 
 const router = express.Router();
 
 export default (): Router => {
-  router.get('/', getMessageIndex);
-  router.get('/helloworld', getMessageHelloWorld);
+  router.get('/', handleIndex);
+  router.get('/name/:name', handleName);
   router.get('*', getResourceNotFound);
   return router;
 };
