b5c60eafbf7276499e1663feae9ea488de550560
wisniewskikr <wisniewskikr@gmail.com>
2023-11-27 12:16:41 +0100

    Add body-parser

--- architectures/gui/html/ts-express-gui-html-helloworld-crud/src/index.ts
+++ architectures/gui/html/ts-express-gui-html-helloworld-crud/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Express } from "express";
+import bodyParser from "body-parser";
 import router from "./router";
 const port = 3000;
 
@@ -8,6 +9,8 @@ app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
 
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('static'));
 app.set('view engine', 'ejs');
 app.use('/', router());
